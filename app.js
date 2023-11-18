705eaa78c834cba43eaa98373d6e907ee6ba80c3
Ignacio Mercado <ignamercado1@gmail.com>
2023-11-18 18:48:34 -0300

    modificaciones entrega final

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const formulario = document.getElementById('miFormulario');
 formulario.addEventListener('submit', function (e) {
     e.preventDefault();
 
-
-// De esta manera accedo a los valores de los campos del formulario
+    // De esta manera accedo a los valores de los campos del formulario
     const nombre = formulario.elements.nombre.value;
     const direccion = formulario.elements.direccion.value;
     const telefono = formulario.elements.telefono.value;
@@ -17,25 +16,20 @@ formulario.addEventListener('submit', function (e) {
         telefono,
     };
 
-// Envío los datos del comprador al sS, así una vez que cierre el navegador, se elimine la información
-
+    // Envío los datos del comprador al sS, así una vez que cierre el navegador, se elimine la información
     sessionStorage.setItem('datosComprador', JSON.stringify(datosComprador));
 
     setTimeout(function () {
-        console.log(`Nombre:\n${nombre}\nDirección:\n${direccion}\nTeléfono:\n${telefono}`);
-        console.log("Los datos han sido enviados correctamente")
         formulario.reset();
     }, 1000);
 });
 
-
-
 // Inicio variables
-
 const carrito = [];
 
-function agregarAlCarrito(producto) {
-    carrito.push(producto);
+/*utilizo desestructuración para obtener directamente las propiedades nombre y precio del producto*/
+function agregarAlCarrito({ nombre, precio }) {
+    carrito.push({ nombre, precio });
     guardarCarritoEnLocalStorage();
     mostrarCarrito();
 }
@@ -44,18 +38,32 @@ function guardarCarritoEnLocalStorage() {
     localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
-// Capturar el boton de comprar
+// Cargo el JSON de productos. Obtengo datos del json mediante fetch
+async function obtenerDatos() {
+    try {
+        const respuesta = await fetch("productos.json");
+        const productos = await respuesta.json();
+        manejarProductos(productos);
+    } catch (error) {
+    }
+}
 
-const buttons = document.querySelectorAll('[id="botonCompra"]');
+// Función para manejar los productos después de la carga
+function manejarProductos(productos) {
+    // Capturo los botones de compra
+    const buttons = document.querySelectorAll('.botonCompra');
 
-// Aquí ejecuto un forEach para recorrer cada elemento de la NodeList llamada -buttons-
-
-buttons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      agregarAlCarrito([camisetas, shorts, medias, botines, guantes][index]);
+    // Asigno un evento a cada botón
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            const productoSeleccionado = productos[index];
+            agregarAlCarrito(productoSeleccionado);
+        });
     });
-});
-  
+}
+
+// Llamo a la función para obtener datos
+obtenerDatos();
 
 function mostrarCarrito() {
     const listaCarrito = document.getElementById('listaCarrito');
@@ -76,7 +84,7 @@ function mostrarCarrito() {
     listaCarrito.innerHTML = carritoHTML;
     totalCarrito.textContent = precioTotal;
 
-    // Escuchar eventos de clic en los botones de eliminar
+    // Escucho eventos de click en los botones de eliminar
     const botonesEliminar = document.querySelectorAll('.eliminarProducto');
     botonesEliminar.forEach((boton) => {
         boton.addEventListener('click', function () {
@@ -91,14 +99,35 @@ function eliminarProductoDelCarrito(index) {
     mostrarCarrito();
 }
 
+/*Capturo el boton para confirmar la compra*/ 
+
+const btnCompra = document.querySelector("#btnConfCompra");
+
+btnCompra.addEventListener("click", () => {
+    function confirmarCompra() {
+        resetearCarrito();
+        Swal.fire({
+            title: "Perfecto!",
+            text: "Haz efectuado la compra con éxito",
+            icon: "success"
+          });
+    }
+    
+    function resetearCarrito() {
+        carrito.length = 0;
+        guardarCarritoEnLocalStorage();
+        mostrarCarrito();
+    }
+
+    // Llamada a la función confirmarCompra
+    confirmarCompra();
+});
 
 function mostrarDatosFinales() {
     const datosComprador = JSON.parse(sessionStorage.getItem('datosComprador'));
     const carrito = JSON.parse(localStorage.getItem('carrito'));
 
     if (datosComprador && carrito) {
-        console.log('Datos del comprador:', datosComprador);
-        console.log('Productos comprados:', carrito);
 
         // Limpio el localStorage después de mostrar los datos 
         sessionStorage.removeItem('datosComprador');
